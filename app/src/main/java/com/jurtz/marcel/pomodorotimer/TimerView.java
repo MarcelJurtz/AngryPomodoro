924c8eed9d66126b09e3c9c0aa96228eb179004a
MarcelJurtz <jurtzmarcel@gmail.com>
2017-09-23 12:58:08 +0200

    Fix layout issues

--- app/src/main/java/com/jurtz/marcel/pomodorotimer/TimerView.java
+++ app/src/main/java/com/jurtz/marcel/pomodorotimer/TimerView.java
@@ -15,5 +15,6 @@ public class TimerView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_timer_view);
+        this.getSupportActionBar().hide();
     }
 }
